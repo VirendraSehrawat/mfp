be8f7e4bbc314c484ee463521bfa06acf23020b1
Virendra Sehrawat <virendra.sehrawat@tavant.com>
2021-01-17 11:08:10 +0530

    initial setup

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+// mount function to start the app
+const mount = (el) => {
+    ReactDOM.render(<App/>, el);
+};
+// if we are running in development and in isolation,
+// call the mount immediately
+if(process.env.NODE_ENV == 'development'){
+    const element = document.querySelector('#_markerting-id-root');
+    if(element){
+        mount(element);
+    }
+}
+// we are running through container
+// and we should export the mount function
+export { mount }
